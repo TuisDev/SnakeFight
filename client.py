f19cc62b21e59b802414fbc31541b77ce2176a41
Tuis <99097079+TuisDev@users.noreply.github.com>
2022-06-18 13:25:14 -0400

    Cleaned code

--- client.py
+++ client.py
@@ -1,20 +1,21 @@
 
-import socket	#for sockets
+import socket
 import sys
 
 computer_name = socket.gethostname()
-ipv6_host = "ff02::abcd:1"
+ipv6_multicast = "ff02::face:1"
 ipv6_port = 65431
+ipv4_host = ''
+ipv4_port = 65432
 
 with socket.socket(socket.AF_INET6, socket.SOCK_DGRAM) as udp_sock:
-    udp_sock.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_MULTICAST_LOOP, True)
 
     while True:
-        udp_sock.sendto("I want to play snake".encode(), (ipv6_host, ipv6_port))
+        udp_sock.sendto("I want to play snake".encode(), (ipv6_multicast, ipv6_port))
         received = udp_sock.recvfrom(1024)
         data = received[0].decode()
         addr = received[1]
-        server_name, ip = data.split(', ')
+        server_name, ipv4_host = data.split(', ')
 
         if received:
             print(f'Play on {server_name}')
@@ -22,7 +23,7 @@ with socket.socket(socket.AF_INET6, socket.SOCK_DGRAM) as udp_sock:
 
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-print((ip, 65432))
-s.connect((ip, 65432))
+print((ipv4_host, ipv4_port))
+s.connect((ipv4_host, ipv4_port))
 s.close()
 # cd onedrive/documents/coding/python
--- server.py
+++ server.py
@@ -1,22 +1,19 @@
-'''
-	Simple udp socket server
-'''
 
 import socket
 import struct
 import sys
 
 computer_name = socket.gethostname()
-ipv6_host = '111111'
+ipv6_multicast = "ff02::face:1"
 ipv6_port = 65431
 ipv4_host = socket.gethostbyname(computer_name)
+ipv4_port = 65432
 
 
 with socket.socket(socket.AF_INET6, socket.SOCK_DGRAM, socket.IPPROTO_UDP) as udp_sock:
     udp_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     udp_sock.bind(('', ipv6_port))
-    udp_sock.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_MULTICAST_LOOP, True)
-    mreq = struct.pack("16s15s".encode('utf-8'), socket.inet_pton(socket.AF_INET6, "ff02::abcd:1"), (chr(0) * 16).encode('utf-8'))
+    mreq = struct.pack("16s15s".encode('utf-8'), socket.inet_pton(socket.AF_INET6, ipv6_multicast), (chr(0) * 16).encode('utf-8'))
     udp_sock.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_JOIN_GROUP, mreq)
 
     while True:
@@ -28,8 +25,8 @@ with socket.socket(socket.AF_INET6, socket.SOCK_DGRAM, socket.IPPROTO_UDP) as ud
 
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-print((ipv4_host, 65432))
-s.bind((ipv4_host, 65432))
+print((ipv4_host, ipv4_port))
+s.bind((ipv4_host, ipv4_port))
 s.listen()
 conn, adr = s.accept()
 s.close()
