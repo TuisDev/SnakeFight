f97dd4b4691a192085794f9cd1ae6c2e627f2965
Tuis <99097079+TuisDev@users.noreply.github.com>
2022-06-18 20:37:40 -0400

    removed pycache

--- server.py
+++ server.py
@@ -11,21 +11,27 @@ ipv4_host = socket.gethostbyname(computer_name)
 ipv4_port = 65432
 start_time = time.time()
 
-with socket.socket(socket.AF_INET6, socket.SOCK_DGRAM, socket.IPPROTO_UDP) as udp_sock, socket.socket(socket.AF_INET, socket.SOCK_STREAM) as tcp_sock:
-    udp_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    udp_sock.setblocking(False)
-    udp_sock.bind(('', ipv6_port))
-    mreq = struct.pack("16s15s".encode('utf-8'), socket.inet_pton(socket.AF_INET6, ipv6_multicast), (chr(0) * 16).encode('utf-8'))
-    udp_sock.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_JOIN_GROUP, mreq)
 
-    while True:
-        data, addr = udp_sock.recvfrom(1024)
-        
-        if data == b'I want to play snake':
-            udp_sock.sendto(f'{computer_name}, {ipv4_host}'.encode(), addr)
-            print(f'send data to {addr}')
-            if time.time() -start_time > 6:
-                break
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as tcp_sock:
+    tcp_sock.bind((ipv4_host, ipv4_port))
+    tcp_sock.setblocking(False)
+    tcp_sock.listen()
+
+    with socket.socket(socket.AF_INET6, socket.SOCK_DGRAM, socket.IPPROTO_UDP) as udp_sock:
+        udp_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        udp_sock.bind(('', ipv6_port))
+        udp_sock.setblocking(False)
+        mreq = struct.pack("16s15s".encode('utf-8'), socket.inet_pton(socket.AF_INET6, ipv6_multicast), (chr(0) * 16).encode('utf-8'))
+        udp_sock.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_JOIN_GROUP, mreq)
+
+        while True:
+            data, addr = udp_sock.recvfrom(1024)
+            
+            if data == b'I want to play snake':
+                udp_sock.sendto(f'{computer_name}, {ipv4_host}'.encode(), addr)
+                print(f'send data to {addr}')
+                if time.time() -start_time > 6:
+                    break
 
 # s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # print((ipv4_host, ipv4_port))
