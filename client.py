99c3aa95c3f6f10c3e2029b5bf051c25b468a794
Tuis <99097079+TuisDev@users.noreply.github.com>
2022-06-19 12:55:34 -0400

    Add multiple connections per server

--- client.py
+++ client.py
@@ -3,28 +3,37 @@ import socket
 import sys
 import pygame as pg
 from game import player_color
+import threading
+import time
 
 computer_name = socket.gethostname()
 ipv6_multicast = "ff02::face:1"
 ipv6_port = 65431
-ipv4_host = ''
+ipv4_host = [None, None]
 ipv4_port = 65432
+start_time = time.time()
 
 def find_games():
     addr_lst = list()
     socket.setdefaulttimeout(1)
     with socket.socket(socket.AF_INET6, socket.SOCK_DGRAM) as udp_sock:
 
-        while True:
-            udp_sock.sendto("I want to play snake".encode(), (ipv6_multicast, ipv6_port))
-            received = udp_sock.recvfrom(1024)
-            data = received[0].decode()
-            addr = received[1]
-            server_name, ipv4_host = data.split(', ')
+        try:
+            while True:
+                udp_sock.sendto("I want to play snake".encode(), (ipv6_multicast, ipv6_port))
+                received = udp_sock.recvfrom(1024)
+                data = received[0].decode()
+                addr = received[1]
+                server_name, ipv4_host[0] = data.split(', ')
 
-            if received is not None and addr not in addr_lst:
-                print(f'Play on {server_name}')
-                addr_lst.append(addr)
+                if received is not None and addr not in addr_lst:
+                    print(f'Play on {server_name}')
+                    addr_lst.append(addr)
+                if time.time() - start_time > 2:
+                    return
+        except:
+            return
+            
 
 def color_picker():
     pass
@@ -32,10 +41,19 @@ def color_picker():
 def main():
     pass
 
-find_games()
+udp = threading.Thread(target=find_games(), args=(), daemon = True)
+udp.start()
 
-# s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# print((ipv4_host, ipv4_port))
-# s.connect((ipv4_host, ipv4_port))
-# s.close()
+
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    print((ipv4_host[0], ipv4_port))
+    s.connect((ipv4_host[0], ipv4_port))
+    while True:
+        s.sendall(b'BANANANANA')
+        data = s.recv(1024)
+
+        print(f'Received from the server : {data.decode()}')
+
+        time.sleep(1)
 # cd onedrive/documents/coding/python
--- server.py
+++ server.py
@@ -3,6 +3,7 @@ import socket
 import struct
 import time
 import sys
+import threading
 
 computer_name = socket.gethostname()
 ipv6_multicast = "ff02::face:1"
@@ -11,16 +12,11 @@ ipv4_host = socket.gethostbyname(computer_name)
 ipv4_port = 65432
 start_time = time.time()
 
-
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as tcp_sock:
-    tcp_sock.bind((ipv4_host, ipv4_port))
-    tcp_sock.setblocking(False)
-    tcp_sock.listen()
-
+def look_for_clients():
     with socket.socket(socket.AF_INET6, socket.SOCK_DGRAM, socket.IPPROTO_UDP) as udp_sock:
         udp_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         udp_sock.bind(('', ipv6_port))
-        udp_sock.setblocking(False)
+        # udp_sock.setblocking(False)
         mreq = struct.pack("16s15s".encode('utf-8'), socket.inet_pton(socket.AF_INET6, ipv6_multicast), (chr(0) * 16).encode('utf-8'))
         udp_sock.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_JOIN_GROUP, mreq)
 
@@ -30,12 +26,41 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as tcp_sock:
             if data == b'I want to play snake':
                 udp_sock.sendto(f'{computer_name}, {ipv4_host}'.encode(), addr)
                 print(f'send data to {addr}')
-                if time.time() -start_time > 6:
-                    break
 
-# s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# print((ipv4_host, ipv4_port))
-# s.bind((ipv4_host, ipv4_port))
-# s.listen()
-# conn, adr = s.accept()
-# s.close()
+def tcp_server(conn):
+
+    while True:
+        data_lock.acquire()
+        data = conn.recv(1024)
+    
+        if data is None:
+            data_lock.release()
+            break
+            
+        conn.sendall(data)
+        data_lock.release()
+        time.sleep(0.1)
+    conn.close()
+
+
+
+if __name__ == '__main__':
+    threading.Thread(target=look_for_clients, args=(), daemon=True).start()
+    
+
+
+    data_lock = threading.Lock()
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        try:
+            print((ipv4_host, ipv4_port))
+            s.bind((ipv4_host, ipv4_port))
+            s.listen()
+            while True:
+                conn, addr = s.accept()
+                print(f'Connected at {addr}')
+                threading.Thread(target=tcp_server, args=(conn,), daemon=True).start()
+                if start_time - time.time() > 15:
+                    break
+        except KeyboardInterrupt:
+            print('BYEBYEBYE')
